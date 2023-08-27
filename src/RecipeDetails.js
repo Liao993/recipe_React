20512641139a96c7c97dbf3c7782328e381cef27
Hung-En Liao <Hung-En Liao>
2023-08-27 09:49:48 -0300

    recipe-details

--- src/RecipeDetails.js
+++ src/RecipeDetails.js
@@ -1,11 +1,23 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const RecipeDetails = () => {
   const { id } = useParams();
+  const { data, isPending, error } = useFetch(
+    "http://localhost:8000/recipes/" + id
+  );
 
   return (
-    <div className="recipe-Details">
-      <h2>{id}</h2>
+    <div className="recipe-details">
+      {isPending && <div>Loading ...</div>}
+      {error && <div>{error}</div>}
+      {data && (
+        <article>
+          <h2> {data.title}</h2>
+          <p>Written by {data.author}</p>
+          <div>{data.body}</div>
+        </article>
+      )}
     </div>
   );
 };
