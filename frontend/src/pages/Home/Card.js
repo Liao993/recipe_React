5994d4e8b90f1ce410ba4681cd8930c8926fd67e
Hung-En Liao <Hung-En Liao>
2023-11-26 10:44:09 -0400

    grid css design

--- frontend/src/pages/Home/Card.js
+++ frontend/src/pages/Home/Card.js
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 
-const Card = ({ _id, dish }) => {
+const Card = ({ _id, dish, country }) => {
   return (
-    <div className="recipe-preview" key={_id}>
+    <div className="recipe-card" key={_id}>
       <Link to={`/recipes/${_id}`}></Link>
       <h2>{dish}</h2>
+      <h3>{country}</h3>
     </div>
   );
 };
--- frontend/src/pages/Home/RecipeList.js
+++ frontend/src/pages/Home/RecipeList.js
@@ -4,34 +4,38 @@ function RecipeList({ recipes, query, title }) {
   return (
     <div className="recipe-list">
       <h2>{title}</h2>
-      {recipes
+      <div className="recipe-preview">
+        {recipes
 
-        // eslint-disable-next-line array-callback-return
-        .filter((post) => {
-          // if search is empty, then return all recipes, else return filtered data
-          if (query === "") {
-            return post;
-          } else if (
-            post.category.toLowerCase().includes(query.toLowerCase())
-          ) {
-            return post;
-          } else if (post.dish.toLowerCase().includes(query.toLowerCase())) {
-            return post;
-          } else if (post.country.toLowerCase().includes(query.toLowerCase())) {
-            return post;
-          } else if (
-            post.ingredient.toLowerCase().includes(query.toLowerCase())
-          ) {
-            return post;
-          }
-        })
+          // eslint-disable-next-line array-callback-return
+          .filter((post) => {
+            // if search is empty, then return all recipes, else return filtered data
+            if (query === "") {
+              return post;
+            } else if (
+              post.category.toLowerCase().includes(query.toLowerCase())
+            ) {
+              return post;
+            } else if (post.dish.toLowerCase().includes(query.toLowerCase())) {
+              return post;
+            } else if (
+              post.country.toLowerCase().includes(query.toLowerCase())
+            ) {
+              return post;
+            } else if (
+              post.ingredient.toLowerCase().includes(query.toLowerCase())
+            ) {
+              return post;
+            }
+          })
 
-        .map((recipe) => (
-          // map is function like loop, to get all infromation of filtered data, and have link for each recipe
-          // The key prop is important for React to efficiently update and re-render lists of elements.
-          // ... can send all data in recipe
-          <Card key={recipe._id} {...recipe} />
-        ))}
+          .map((recipe) => (
+            // map is function like loop, to get all infromation of filtered data, and have link for each recipe
+            // The key prop is important for React to efficiently update and re-render lists of elements.
+            // ... can send all data in recipe
+            <Card key={recipe._id} {...recipe} />
+          ))}
+      </div>
     </div>
   );
 }
