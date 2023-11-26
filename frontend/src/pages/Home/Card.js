a53937f60a6795f69b05a78b4e01f6815ef6d6a7
Hung-En Liao <Hung-En Liao>
2023-11-26 12:28:57 -0400

    card component css word design

--- frontend/src/pages/Home/Card.js
+++ frontend/src/pages/Home/Card.js
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 
-const Card = ({ _id, dish, country }) => {
+const Card = ({ _id, dish, country, image }) => {
+  console.log(image);
   return (
     <div className="recipe-card" key={_id}>
-      <Link to={`/recipes/${_id}`}></Link>
-      <h2>{dish}</h2>
-      <h3>{country}</h3>
+      <Link to={`/recipes/${_id}`}>
+        <img src={require(`../../assets/img/${image}`)} alt={dish} />
+        <div className="recipe-word">
+          <h2>{dish}</h2>
+          <h3>{country}</h3>
+        </div>
+      </Link>
     </div>
   );
 };
